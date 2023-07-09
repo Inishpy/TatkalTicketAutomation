d3f1fad54fd3f135d98a9db6d34bdfdd2df98a03
Inishpy <62107696+Inishpy@users.noreply.github.com>
2023-07-09 11:48:06 +0530

    first commit

---
+++ tatkalgui.py
@@ -0,0 +1,38 @@
+import keyboard
+import pyautogui
+
+while True:
+    if keyboard.read_key() == ".":
+        pyautogui.typewrite(['backspace'])
+        pyautogui.typewrite('INISH MELROY D S')
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite('21')
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['m'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite('9740052015')
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite('MUKUDA HOUSE')
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite('SORNAD POST')
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite('BANTWAL TALUK')
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite('574211')
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['tab'])
+        pyautogui.typewrite(['enter'])
+        for i in range(15):
+            pyautogui.typewrite(['down'])
+        pyautogui.typewrite(['enter'])
+        
+
+        break
